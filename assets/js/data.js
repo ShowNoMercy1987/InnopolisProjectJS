cef8b50f56f3045577b85802a95c2012372abf76
Dmitriy Ovchinnikov <dlcerberos@gmail.com>
2023-03-16 01:32:00 +0300

    push

---
+++ assets/js/data.js
@@ -0,0 +1,50 @@
+"use strict";
+
+let obj1 = {
+  model: "Apple iPhone 13",
+  color: ["red", "green", "pink", "blue", "white", "black"],
+  selectedColor: "blue",
+  screen: 6.1,
+  modelMemoryGb: [128, 256, 515],
+  selectedMemoryGb: 128,
+  os: "iOS 15",
+  wlanInterface: ["NFC", "Bluetooth", "Wi-Fi"],
+  cpu: "Apple a-15 Bionic",
+  oldPriceRubles: 75990,
+  newPriceRubles: 67990,
+  delivery: [
+    varriant1 = {
+      name: "Самовывоз",
+      date: "Четверг, 1 сентября",
+      const: 0,
+    },
+
+    varriant2 = {
+      name: "Курьером",
+      date: "Четверг, 1 сентября",
+      const: 0,
+    }
+  ]
+}
+
+let obj2 = {
+  name: "Марк Г.",
+  avatar: "./assets/img/review-1.jpeg",
+  rating: 5,
+  exp: "менее месяца",
+  commentPlus: `это мой первый айфон после после огромного количества телефонов на андроиде. 
+  всёплавно, чётко и красиво. довольно шустрое устройство. 
+  камера весьма неплохая,ширик тоже на высоте.`,
+  commentMinus: `к самому устройству мало имеет отношение, 
+  но перенос данных с андроида - адскаявещь) а если нужно переносить фото с компа, 
+  то это только через itunes, которыйурезает качество фотографий исходное`,
+}
+
+let obj3 = {
+  name: "Валерий Коваленко",
+  avatar: "./assets/img/star-5.png",
+  rating: 4,
+  exp: "менее месяца",
+  commentPlus: "OLED экран, Дизайн, Система камер, Шустрый А15, Заряд держит долго",
+  commentMinus: "Плохая ремонтопригодность",
+}
